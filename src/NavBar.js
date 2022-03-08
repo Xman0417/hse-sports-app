788e6f3578c56a407a4c81b9e98e08b87cc69e10
Xman0417 <67702958+Xman0417@users.noreply.github.com>
2022-03-08 10:30:04 -0500

    added documentation for bootstrap

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import { Container } from "react-bootstrap";
 import Nav from 'react-bootstrap/Nav';
+//https://react-bootstrap.github.io/getting-started/introduction/
+
 
 function NavBar(){
     return(
         <Navbar bg="primary" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="#">Navbar</Navbar.Brand>
                     <Nav className="me-auto">
                     <Nav.Link href="#">Home</Nav.Link>
                     <Nav.Link href="#">Features</Nav.Link>
