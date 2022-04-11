8535f3672c29637495c4f4665a1bf090880ec4ef
Xman0417 <67702958+Xman0417@users.noreply.github.com>
2022-04-11 11:05:23 -0400

    Fixed Height on Scores and Tweets

--- src/Scores.js
+++ src/Scores.js
@@ -6,7 +6,6 @@ function Scores(){
     console.log (insertedScript);
     if(insertedScript !== null){
         document.getElementsByTagName('head')[0].removeChild(insertedScript);
-        insertedScript = null;
     }
     return(
         
@@ -14,7 +13,7 @@ function Scores(){
             
             <h2>Scores</h2>
             
-            <div className="scorestream-widget-container" data-ss_widget_type="vertScoreboard" style={{height:"600px"}} data-user-widget-id="49107"></div>
+            <div className="scorestream-widget-container" data-ss_widget_type="vertScoreboard" style={{height:"400px"}} data-user-widget-id="49107"></div>
             <p>NOTE: This may not be the most updated - please check the TWEETS section for the most up to date information.</p>
 
         </div>
