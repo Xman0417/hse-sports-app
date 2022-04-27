521f9833fa8eb82efa62c3a8183830bc1479ccef
Xman0417 <67702958+Xman0417@users.noreply.github.com>
2022-04-27 11:43:31 -0400

    Fixed styling and tested key part of filter

--- src/Tweets.js
+++ src/Tweets.js
@@ -3,17 +3,15 @@ import useScript from "./hooks/useScript";
 
 function Tweets(){
     useScript('https://platform.twitter.com/widgets.js');
-    
+    const fullList = "https://twitter.com/hsesportsapp/lists/1517144091179450368?ref_src=twsrc%5Etfw"
     return(
         <div style={{display: "flex", justifyContent: "center"}}>
            
             
-            <a className="twitter-timeline" data-width="700" data-height="480" data-chrome="nofooter" href="https://twitter.com/hsesportsapp/lists/1517144091179450368?ref_src=twsrc%5Etfw">HSE Sports Twitter List</a>
+            <a className="twitter-timeline" data-width="700" data-height="480" data-chrome="nofooter" href={fullList}>Loading...</a>
             
         </div>
     );
     
 }
 export default Tweets;
-
-//<a class="twitter-timeline" href="https://twitter.com/xman0417/lists/1512592955579768836?ref_src=twsrc%5Etfw">A Twitter List by xman0417</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
