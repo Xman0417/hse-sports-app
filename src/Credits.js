5d1f65760307814e8c96171c31d0cb7c13603610
Xman0417 <67702958+Xman0417@users.noreply.github.com>
2022-05-01 18:32:19 -0400

    forgot one aspect of credits

--- src/Credits.js
+++ src/Credits.js
@@ -8,6 +8,9 @@ function Credits(){
                     The HSE Sports App was developed by Xander Beauchamp and Abhimanyu Chaddha of Software Development. <br /> <br />
                 </p>
                 <p style={{margin: "0px", fontSize: "24px"}}>Questions? Something incorrect? Other feedback? <a href="mailto: beaucxan000@hsestudents.org, chaddabh000@hsestudents.org">Contact Us!</a></p>
+                
+                {/*Required by Twitter*/}
+                <p style={{fontSize:"20px"}}>Tweets came from the <a href="https://twitter.com/i/lists/1517144091179450368?s=20">Royals Sports Twitter List created by @HSESportsApp</a></p>
             
             
         </div>
